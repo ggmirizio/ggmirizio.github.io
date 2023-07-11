cea15077a5aec01516f81f008768c4cec1babcbb
Gerardo Mirizio <gerardo.mirizio@gmail.com>
2023-07-10 20:20:24 -0400

    add photo + update profile

---
+++ Untitled.R
@@ -0,0 +1,8 @@
+## Create an RStudio project first with this particular name
+usethis::create_project("~/Downloads/ggmirizio.github.io")
+
+## Configure Git and GitHub
+## If you don't have git, check "Happy Git and GitHub for the useR" available
+## at https://happygitwithr.com/
+usethis::use_git()
+usethis::use_github()
